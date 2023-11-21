0436662201cd466f2044ed0661fe6b041e0de57d
Константин Денисович Лишенков <s0163557@edu.kubsu.ru>
2023-11-21 03:33:15 +0300

    Реализован CRUD

--- Banking Operations/Controllers/BankingController.cs
+++ Banking Operations/Controllers/BankingController.cs
@@ -9,30 +9,110 @@ namespace Banking_Operations.Controllers
     public class BankingController : Controller
     {
         private IBankServicesRepository _repository;
-        public BankingController(IBankServicesRepository repository) 
+        private UserManager<Client> _clientManager;
+        public BankingController(IBankServicesRepository repository, UserManager<Client> clientManager) 
         {
+            _clientManager = clientManager;
             _repository = repository;
         }
 
         public async Task<IActionResult> Index()
         {
-            IEnumerable<BankService> bankServices = await _repository.GetAll();
+            Client client = await _clientManager.FindByNameAsync(User.Identity.Name);
+            IEnumerable<BankService> bankServices = await _repository.GetByClientIdAsync(client.Id);
             return View(bankServices);
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create(BankService bankService)
+        public async Task<IActionResult> Create(BankingModel bankingModel)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                return View(bankService);
+                return View(bankingModel);
             }
+
+            Client client = await _clientManager.FindByNameAsync(User.Identity.Name);
+
+            BankService bankService = new BankService
+            {
+                BankServiceName = bankingModel.BankServiceName,
+                TermOfRendering = bankingModel.TermOfRendering,
+                ComissionType = bankingModel.ComissionType,
+                Debt = bankingModel.Debt,
+                BankServiceStatus = false,
+                ClientId = client.Id
+            };
             _repository.Add(bankService);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(BankService bankService)
+        {
+            BankService newBankService = await _repository.GetByIdAsync(bankService.Id);
+
+            BankingModel bankingModel = new BankingModel
+            {
+                ServiceID = newBankService.Id,
+                BankServiceName = newBankService.BankServiceName,
+                TermOfRendering = newBankService.TermOfRendering,
+                ComissionType = newBankService.ComissionType,
+                Debt = newBankService.Debt,
+                ClientId = newBankService.ClientId
+            }; 
+            return View(bankingModel);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(BankService bankService)
+        {
+            BankService newBankService = await _repository.GetByIdAsync(bankService.Id);
+
+            BankingModel bankingModel = new BankingModel
+            {
+                ServiceID = newBankService.Id,
+                BankServiceName = newBankService.BankServiceName,
+                TermOfRendering = newBankService.TermOfRendering,
+                ComissionType = newBankService.ComissionType,
+                Debt = newBankService.Debt,
+                ClientId = newBankService.ClientId
+            };
+            return View(bankingModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(BankingModel bankingModel)
+        {
+            Client client = await _clientManager.FindByNameAsync(User.Identity.Name);
+
+            var bankService = new BankService
+            {
+                BankServiceName = bankingModel.BankServiceName,
+                TermOfRendering = bankingModel.TermOfRendering,
+                ComissionType = bankingModel.ComissionType,
+                Debt = bankingModel.Debt,
+                BankServiceStatus = false,
+                ClientId = client.Id
+            };
+            _repository.Update(bankService);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            var bankService = await _repository.GetByIdAsync(id);
+            _repository.Delete(bankService);
             return RedirectToAction("Index");
         }
     }
--- Banking Operations/Models/BankingModel.cs
+++ Banking Operations/Models/BankingModel.cs
@@ -2,10 +2,11 @@
 {
     public class BankingModel
     {
-        public int? ServiceID { get; set; }
+        public int ServiceID { get; set; }
         public string? BankServiceName { get; set; }
         public string? TermOfRendering { get; set; }
         public string? ComissionType { get; set; }
         public int? Debt { get; set; }
+        public string? ClientId { get; set; }
     }
 }
